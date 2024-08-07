be69cb87279d3a3ba42eaba91d36ba65ea1cf192
Raymundo Chavez <raymundo.developer@gmail.com>
2024-08-06 18:44:22 -0600

    Se agrega frame para reproducir los videos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "auto-fill": "repeat(auto-fill, minmax(360px, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
