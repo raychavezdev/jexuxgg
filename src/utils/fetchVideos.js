47ec7790c0219265b0b27bbe36724097118b2668
Raymundo Chavez <raymundo.developer@gmail.com>
2024-08-16 16:40:15 -0600

    Mueve la petición fetch a archivo separado

---
+++ src/utils/fetchVideos.js
@@ -0,0 +1,26 @@
+export async function fetchVideos() {
+  try {
+    const KEY = import.meta.env.KEY
+    const ID = import.meta.env.ID
+    const TOTAL_RES = 9
+
+    const response = await fetch(
+      'https://www.googleapis.com/youtube/v3/search?key=' +
+        KEY +
+        '&channelId=' +
+        ID +
+        '&part=snippet,id&order=date&maxResults=' +
+        TOTAL_RES,
+    )
+
+    if (!response.ok) {
+      throw new Error('Error fetching videos')
+    }
+
+    const data = await response.json()
+    return data.items
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
