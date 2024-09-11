925d42cfffae7ebaa94932a907ee7ebb4e4a5199
Raymundo Chavez <raymundo.developer@gmail.com>
2024-09-11 12:57:05 -0600

    Agrega fuentes personalizadas y scroll-smooth

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        guerrilla: ['Protest Guerrilla', 'system-ui'],
+      },
       gridTemplateColumns: {
         'auto-fill': 'repeat(auto-fill, minmax(280px, 1fr))',
       },
